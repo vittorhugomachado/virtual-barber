59847241225ea9c77985bc92775f668427c5aeee
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2026-02-06 13:00:55 -0300

    prisma e ts configurados

---
+++ apps/api/modules/auth/owner/auth.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { authLimiter } from './auth-limiter.middleware';
+import { authenticateToken } from '../../../middlewares/authenticate-token.middleware';
+import { signUp, login, refreshAccessToken, logout } from './auth.controller';
+
+const router = Router();
+
+router.post('/signup', signUp);
+
+router.post('/login', authLimiter, login);
+
+router.post('/refresh-token', refreshAccessToken);
+
+router.post('/logout', authenticateToken, logout);
+
+export default router;
